f23cac85d6daba19133c803b3c2f254a7b50783e
Wesley Sun <wesleyksun@gmail.com>
2017-02-09 15:09:53 -0800

    initial commit

---
+++ src/foo.py
@@ -0,0 +1,12 @@
+
+def bar(a):
+    if a == 0:
+        return 0
+    elif a == 1:
+        return 2
+    elif a == 2:
+        return 4
+    elif a == 3:
+        return 6
+    else:
+        return 10
---
+++ test/test_foo.py
@@ -0,0 +1,5 @@
+from src import foo
+
+
+def test_bar():
+    assert foo.bar(1) == 2
